cabce1839290dc16ac2ba54689e6d3e1785a545b
Anuar Hernandez <anuarhernandezdelgado@gmail.com>
2022-05-02 15:19:09 -0500

    Tarea lista

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8237313de5a37274c5abc2bf493e91c4'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a970502a-76b5-459a-a2f6-a9b5734a2477'
+};
